b7d6f422f4d5d4f0b035f230a9569849b9ccb40e
Yume Fernandes <yumefernandes.dev@gmail.com>
2025-04-27 21:37:26 -0300

    ROTEAMENTO basico perfil

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Página Inicial' });
+  res.render('login', { title: 'Página Inicial' });
 });
 
 router.get('/about', (req, res) => {
   res.render('about', { title: 'Sobre Nós' });
 });
 
+
+router.get('/profile', (req, res) => {
+    res.render('profile', { title: 'PERFIL' });
+  });
 module.exports = router;
